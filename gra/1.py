2950cb561c0c5e345541d81bd1fd529724d5b76a
Jarogniev <102618797+Jarogniev@users.noreply.github.com>
2025-06-02 22:08:03 +0200

    HoMM 0002 - added player in file 'gracz.py'

--- gra/1.py
+++ gra/1.py
@@ -1,5 +1,8 @@
 import pygame
 
+from gra.gracz import Gracz
+from gra.wrog import Wrog
+
 pygame.init()
 
 # Ustawienia okna gry
@@ -13,47 +16,6 @@ okno_gry = pygame.image.load('skorki/zamek2.jpg')
 # Licznik klatek
 clock = pygame.time.Clock()
 FPS = 60
-
-
-# === Klasy ===
-class Gracz:
-    def __init__(self, x, y, obraz):
-        self.x = x
-        self.y = y
-        self.obraz = pygame.image.load(obraz)
-        self.ruchX = 0
-        self.ruchY = 0
-        self.predkosc = 1
-
-    def rysuj(self, ekran):
-        ekran.blit(self.obraz, (self.x, self.y))
-
-    def sterowanie(self, klawisze):
-        if klawisze[pygame.K_a]:
-            self.x -= self.predkosc
-        if klawisze[pygame.K_d]:
-            self.x += self.predkosc
-        if klawisze[pygame.K_w]:
-            self.y -= self.predkosc
-        if klawisze[pygame.K_s]:
-            self.y += self.predkosc
-
-        self.x += self.ruchX
-        self.y += self.ruchY
-
-        # Ograniczenia ekranu
-        self.x = max(0, min(self.x, 1150))
-        self.y = max(0, min(self.y, 760))
-
-
-class Wrog:
-    def __init__(self, x, y, obraz):
-        self.x = x
-        self.y = y
-        self.obraz = pygame.image.load(obraz)
-
-    def rysuj(self, ekran):
-        ekran.blit(self.obraz, (self.x, self.y))
 
 
 # === Tworzenie obiektów ===
@@ -73,13 +35,13 @@ while gra:
             gra = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                gracz.ruchY += 0.3
+                gracz.ruchY += 1.5
             if event.key == pygame.K_UP:
-                gracz.ruchY -= 0.3
+                gracz.ruchY -= 1.5
             if event.key == pygame.K_RIGHT:
-                gracz.ruchX += 0.3
+                gracz.ruchX += 1.5
             if event.key == pygame.K_LEFT:
-                gracz.ruchX -= 0.3
+                gracz.ruchX -= 1.5
 
     klawisze = pygame.key.get_pressed()
 
--- gra/gracz.py
+++ gra/gracz.py
@@ -1,1 +1,31 @@
 # cały gracz
+import pygame
+class Gracz:
+    def __init__(self, x, y, obraz):
+        self.x = x
+        self.y = y
+        self.obraz = pygame.image.load(obraz)
+        self.ruchX = 0
+        self.ruchY = 0
+        self.predkosc = 1
+
+    def rysuj(self, ekran):
+        ekran.blit(self.obraz, (self.x, self.y))
+
+    def sterowanie(self, klawisze):
+        if klawisze[pygame.K_a]:
+            self.x -= self.predkosc
+        if klawisze[pygame.K_d]:
+            self.x += self.predkosc
+        if klawisze[pygame.K_w]:
+            self.y -= self.predkosc
+        if klawisze[pygame.K_s]:
+            self.y += self.predkosc
+
+        self.x += self.ruchX
+        self.y += self.ruchY
+
+        # Ograniczenia ekranu
+        self.x = max(0, min(self.x, 1150))
+        self.y = max(0, min(self.y, 760))
+
